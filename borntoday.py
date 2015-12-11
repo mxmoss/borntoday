37f87c95d374234413a7e507ffa2d090a5869179
mxmoss <mxmossman@gmail.com>
2015-12-11 15:02:06 -0800

    Update borntoday.py
    dynamically calculate today's date

--- borntoday.py
+++ borntoday.py
@@ -10,8 +10,12 @@ import urllib.request		# this for python 3 and later
 import requests
 
 # Wikipedia has a page for each day of the year eg: https://en.wikipedia.org/wiki/December_11
-myURL = "en.wikipedia.org/wiki/December_11"
-page = urllib.request.urlopen("http://"+myURL)
+myURL = "en.wikipedia.org/wiki/"
+
+#get today's date
+today = datetime.date.today()
+myDate = today.strftime('%B_%d')
+page = urllib.request.urlopen("http://"+myURL+myDate)
 
 #parse
 soup = BeautifulSoup(page, "html.parser")
@@ -20,9 +24,7 @@ soup = BeautifulSoup(page, "html.parser")
 rows = soup.find_all('li' )
 
 #output
-print('<html><head><title>Events for December 11</title></head><body><ul>')
+print('<html><head><title>Events for {0}</title></head><body><ul>'.format(today))
 for row in rows:
   print('<li>{0}</li>'.format(row.text))
 print('</ul></body></html>')
-
-
