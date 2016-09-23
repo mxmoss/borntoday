57f1fe7f6ec2514566a0cfb469549e252c9cd81f
Moss Drake <mxmossman@gmail.com>
2016-09-23 16:17:16 -0700

    Successfully makes the list of events.
    Previously this was barely working. Now it works to generate a list of
    events per day. Still to do: only show certain entries

--- README.md
+++ README.md
@@ -4,3 +4,14 @@ Python script to extract a list of who was born or died on a specific date and o
 Wikipedia has a page for each day of the year eg: https://en.wikipedia.org/wiki/December_11
 
 Use this to generate the list
+
+
+Notes:
+I had problems running this due to Unicode error messages
+  "UnicodeEncodeError: 'charmap' codec can't encode character ";
+The suggested solution is to run the console as unicode
+  py -mpip install win-unicode-console
+  py -mrun your_script.py
+or, to replace any unicode characters with an empty string
+  set PYTHONIOENCODING=:replace
+http://stackoverflow.com/questions/5419/python-unicode-and-the-windows-console/32176732#32176732
--- borntoday.py
+++ borntoday.py
@@ -15,16 +15,38 @@ myDate = today.strftime('%B_%d')
 
 # Wikipedia has a page for each day of the year eg: https://en.wikipedia.org/wiki/December_11
 myURL = "http://en.wikipedia.org/wiki/"+myDate
+print(myURL)
 page = urllib.request.urlopen(myURL)
 
 #parse
 soup = BeautifulSoup(page, "html.parser")
 
-# Extract all list items <li>
-rows = soup.find_all('li' )
+# Extract all list items <ul>
+uSections = soup.find_all('ul')
+words = ['math', 'scientist', 'programm']
 
-#output
-print('<html><head><title>Events for {0}</title></head><body><ul>'.format(today))
-for row in rows:
-  print('<li>{0}</li>'.format(row.text))
-print('</ul></body></html>')
+print('<html><head><title>Events for {0}</title></head><body>'.format(today))
+
+'''
+# print out sections
+# skip first <ul> entry
+for uSection in uSections[1:]:
+  print('{0}'.format(uSection))
+'''
+
+
+for iEvents in uSections[1:4]:
+  print('{0}'.format(iEvents.find_all('li')))
+  
+'''
+# print out items that match
+for iEvents in uSections[1:4]:
+  print('hey!{0}<br/>'.format(iEvents))
+  for stopword in words:
+    print(stopword)
+    if stopword in iEvents: #words in li:
+      print('ho!')
+      print('{0}'.format(iEvents))
+'''
+
+print('</body></html>')
