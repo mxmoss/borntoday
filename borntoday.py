56f767069e37df739c7f1b3ba1ebdf8c56f74cf4
Moss Drake <mxmossman@gmail.com>
2023-05-04 13:06:19 -0700

    Update borntoday.py
    1) Extract the stopwords to a "global constant"
    2) Remove the bounds on iEvents - this is to adjust for Wikipedia html changes
    3) Refactor printing html header
    4) break out of stopwords loop - to avoid duplicates

--- borntoday.py
+++ borntoday.py
@@ -10,6 +10,8 @@ import datetime
 import urllib.request		# this for python 3 and later
 import requests
 
+CONST_WORDS = ['mathe', 'scientist', 'programm', 'comput']
+
 def BornDied(aDate):
   myDate = aDate.strftime('%B_%d')
   # Wikipedia has a page for each day of the year eg: https://en.wikipedia.org/wiki/December_11
@@ -21,21 +23,21 @@ def BornDied(aDate):
 
   # Extract all list items <ul>
   uSections = soup.find_all('ul')
-  words = ['mathe', 'scientist', 'programm', 'comput']
 
   # extract all 4 sections
   sItems = []
-  for iEvents in uSections[1:4]:
+  for iEvents in uSections[14:]:
     snippet = iEvents.find_all('li')
     sItems = sItems + snippet
     
   #spew them out, but only if they contain keywords
-  print('<html><head><base href="http://en.wikipedia.org/."> <title>Events for {0}</title></head><body>'.format(aDate))
-  print(myURL)
+  print('<html><head><base href="{0}"> <title>Events for {1}</title></head><body>'.format(myURL, aDate))
   for sItem in sItems:
-    for stopword in words:
+    for stopword in CONST_WORDS:
       if stopword in sItem.get_text():
         print('{0}'.format(sItem))
+        break
+
 #        print('{0}'.format(sItem).encode('ascii','ignore'))
   print('</body></html>')
 
